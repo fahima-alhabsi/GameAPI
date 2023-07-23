8cc069ab80921868c5f282dfd5fc10aa0db8e6e6
unknown <fahima-alhabsi>
2023-07-23 20:17:08 +0400

    add proprites

--- src/main/java/com/fahima/gameAPI/Models/playerModel.java
+++ src/main/java/com/fahima/gameAPI/Models/playerModel.java
@@ -1,4 +1,16 @@
 package com.fahima.gameAPI.Models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class playerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public int age ;
+    public String nationality ;
+
+
 }
